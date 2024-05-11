23bdb0f18dd88dfdd5f777fb415a2de54fdb6acb
frj <1712963739@qq.com>
2024-05-11 17:18:15 +0800

    update：     thread-test/Client.c 	update：     thread-test/Server.c 	update：     thread-test/bulid/client 	update：     thread-test/bulid/server

--- thread-test/Client.c
+++ thread-test/Client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -8,10 +9,80 @@
 
 #define BUFSZ 256
 
+void trim(char *str);
+
 typedef struct message_t {
     char msg_type[BUFSZ];
     char msg_text[BUFSZ];
 }message_t;
+
+void *send_task(void *arg)
+{
+    int qid;
+    key_t key;
+    message_t *msg = (message_t *)arg;
+
+    if ((key = ftok(".", 'c')) == -1)
+        perror("ftok");
+
+    if ((qid = msgget(key, IPC_CREAT | 0666)) == -1)
+        perror("msgget");
+
+    printf("Enter your client type: ");
+    fgets(msg->msg_type, BUFSZ, stdin);
+    trim(msg->msg_type);
+
+    while (1)
+    {
+        // printf("\nEnter message: ");
+        fgets(msg->msg_text, BUFSZ, stdin);
+
+        if (msgsnd(qid, msg, sizeof(message_t), 0) == -1)
+            perror("msgsnd");
+
+        trim(msg->msg_text);
+
+        if (!strcmp(msg->msg_text, "exit"))
+            exit(0);
+    }
+
+    printf("Thread 1 is exiting\n");
+    pthread_exit(NULL);
+}
+
+void *srv_task(void *arg)
+{
+    int qid;
+    key_t key;
+    message_t rsv_msg;
+    message_t *msg = (message_t *)arg;
+
+    if ((key = ftok(".", 's')) == -1)
+        perror("ftok");
+
+    if ((qid = msgget(key, IPC_CREAT | 0666)) == -1)
+        perror("msgget");
+
+    while (1)
+    {
+        if (msgrcv(qid, &rsv_msg, sizeof(message_t), 0, 0) == -1)
+            perror("msgrcv");
+
+        trim(msg->msg_type);
+        trim(rsv_msg.msg_type);
+        if(strcmp(msg->msg_type, rsv_msg.msg_type))
+            printf("\r\n%s: %s", rsv_msg.msg_type, rsv_msg.msg_text);
+
+
+        trim(rsv_msg.msg_text);
+        if (!strcmp(rsv_msg.msg_text, "exit"))
+            exit(0);
+    }
+
+    printf("Thread 1 is exiting\n");
+    pthread_exit(NULL);
+
+}
 
 // 去除字符串中的空白字符
 void trim(char *str)
@@ -26,31 +97,39 @@ void trim(char *str)
 
 int main()
 {
-    int qid;
-    key_t key;
+    pthread_t send_thread, srv_thread;
+    int ret1, ret2;
     message_t msg;
-
-    if ((key = ftok(".", 'a')) == -1)
-        perror("ftok");
-
-    if ((qid = msgget(key, 0666)) == -1)
-        perror("msgget");
-
-    printf("Enter your client type: ");
-    fgets(msg.msg_type, BUFSZ, stdin);
-    trim(msg.msg_type);
-    while (1)
+    // 创建线程1
+    ret1 = pthread_create(&send_thread, NULL, send_task, &msg);
+    if (ret1)
     {
-        printf("Enter message: ");
-        fgets(msg.msg_text, BUFSZ, stdin);
+        fprintf(stderr, "Error - pthread_create() return code: %d\n", ret1);
+        return 1;
+    }
 
-        if (msgsnd(qid, &msg, sizeof(message_t), 0) == -1)
-            perror("msgsnd");
-        
-        trim(msg.msg_text);
-        
-        if (!strcmp(msg.msg_text, "exit"))
-            exit(0);
+    // 创建线程2
+    ret2 = pthread_create(&srv_thread, NULL, srv_task, &msg);
+    if (ret2)
+    {
+        fprintf(stderr, "Error - pthread_create() return code: %d\n", ret2);
+        return 1;
+    }
+
+    // 等待线程1结束
+    ret1 = pthread_join(send_thread, NULL);
+    if (ret1)
+    {
+        fprintf(stderr, "Error - pthread_join() return code: %d\n", ret1);
+        return 1;
+    }
+
+    // 等待线程2结束
+    ret2 = pthread_join(srv_thread, NULL);
+    if (ret2)
+    {
+        fprintf(stderr, "Error - pthread_join() return code: %d\n", ret2);
+        return 1;
     }
 
     return 0;
--- thread-test/Server.c
+++ thread-test/Server.c
@@ -1,12 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <string.h>
 #include <ctype.h>
 
-#define BUFSZ 256
+#define BUFSZ           256
+#define CLIENT_COUNT    3
+
+typedef struct message_t {
+    char msg_type[BUFSZ];
+    char msg_text[BUFSZ];
+}message_t;
+
+
+void trim(char *str);
+
+void *srv_task(void *arg)
+{
+    // 执行一些任务...
+    int srv_qid, send_qid;
+    key_t srv_key, send_key;
+    message_t msg;
+
+    if ((srv_key = ftok(".", 'c')) == -1)
+        perror("ftok");
+
+    if ((srv_qid = msgget(srv_key, IPC_CREAT | 0666)) == -1)
+        perror("msgget");
+
+    if ((send_key = ftok(".", 's')) == -1)
+        perror("ftok");
+
+    if ((send_qid = msgget(send_key, IPC_CREAT | 0666)) == -1)
+        perror("msgget");
+
+    while (1)
+    {
+        if (msgrcv(srv_qid, &msg, sizeof(message_t), 0, 0) == -1)
+            perror("msgrcv");
+
+        printf("%s: %s", msg.msg_type, msg.msg_text);
+
+        for (int i = 0;i < CLIENT_COUNT;i++)
+        {
+            if (msgsnd(send_qid, &msg, sizeof(message_t), 0) == -1)
+                perror("msgsnd");
+        }
+
+
+        trim(msg.msg_text);
+
+        if (!strcmp(msg.msg_text, "exit"))
+            exit(0);
+    }
+
+    printf("Thread 1 is exiting\n");
+    pthread_exit(NULL);
+
+}
 
 // 去除字符串中的空白字符
 void trim(char *str)
@@ -19,34 +73,25 @@ void trim(char *str)
     }
 }
 
-typedef struct message_t {
-    char msg_type[BUFSZ];
-    char msg_text[BUFSZ];
-}message_t;
-
 int main()
 {
-    int qid;
-    key_t key;
-    message_t msg;
+    pthread_t send_thread, srv_thread;
+    int ret2;
 
-    if ((key = ftok(".", 'a')) == -1)
-        perror("ftok");
-
-    if ((qid = msgget(key, IPC_CREAT | 0666)) == -1)
-        perror("msgget");
-
-    while (1)
+    // 创建线程2
+    ret2 = pthread_create(&srv_thread, NULL, srv_task, NULL);
+    if (ret2)
     {
-        if (msgrcv(qid, &msg, sizeof(message_t), 0, 0) == -1)
-            perror("msgrcv");
-        
-        printf("%s: %s", msg.msg_type, msg.msg_text);
-        
-        trim(msg.msg_text);
+        fprintf(stderr, "Error - pthread_create() return code: %d\n", ret2);
+        return 1;
+    }
 
-        if(!strcmp(msg.msg_text, "exit"))
-            exit(0);
+    // 等待线程2结束
+    ret2 = pthread_join(srv_thread, NULL);
+    if (ret2)
+    {
+        fprintf(stderr, "Error - pthread_join() return code: %d\n", ret2);
+        return 1;
     }
 
     return 0;
